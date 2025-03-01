7983ff3c0f8bc667d8c8557bc7000b337dfed461
Onur Çakılı <73158910+onurcakili@users.noreply.github.com>
2025-03-01 18:18:01 +0300

    Update 04_1_cltv.py

--- 01_Data_Science_Projects/04_1_cltv.py
+++ 01_Data_Science_Projects/04_1_cltv.py
@@ -2,15 +2,15 @@
 # CUSTOMER LIFETIME VALUE (Müşteri Yaşam Boyu Değeri)
 ########################################################################################
 
-# 1. Veri Hazırlama
-# 2. Average Order Value (average_order_value = total_price / total_transaction)
-# 3. Purchase Frequency (total_transaction / total_number_of_customers)
-# 4. Repeat Rate & Churn Rate (birden fazla alışveriş yapan müşteri sayısı / tüm müşteriler)
-# 5. Profit Margin (profit_margin =  total_price * 0.10)
-# 6. Customer Value (customer_value = average_order_value * purchase_frequency)
-# 7. Customer Lifetime Value (CLTV = (customer_value / churn_rate) x profit_margin)
-# 8. Segmentlerin Oluşturulması
-# 9. BONUS: Tüm İşlemlerin Fonksiyonlaştırılması
+# 1. Veri Hazırlama  
+# 2. Average Order Value (Ortalama Sipariş Değeri )       >>>> (average_order_value = total_price / total_transaction)  
+# 3. Purchase Frequency  (Satın Alma Sıklığı)        >>>> (total_transaction / total_number_of_customers)  
+# 4. Repeat Rate & Churn Rate (Tekrar ve Kayıp Orn.)        >>>> (birden fazla alışveriş yapan müşteri sayısı / tüm müşteriler)  
+# 5. Profit Margin (Kâr Marjı)          >>>> (profit_margin =  total_price * 0.10)  
+# 6. Customer Value (Müşteri Değeri)      >>>   (customer_value = average_order_value * purchase_frequency)  
+# 7. Customer Lifetime Value (Müşteri Yaşam Boyu Değeri)        >>>> (CLTV = (customer_value / churn_rate) x profit_margin)  
+# 8. Segmentlerin Oluşturulması  
+
 
 
 
